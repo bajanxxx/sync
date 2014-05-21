9dcf52c671caf3c3065e54de63a9b1c06f253efa
ashrithr <ashrith@me.com>
2014-05-20 19:22:38 -0500

    Add default values to application model

--- models/application.rb
+++ models/application.rb
@@ -6,8 +6,8 @@ class Application
   field :comments, type: Array
   field :notes, type: String
   field :resume_id, type: String
-  field :status, type: Array # [ 'APPLIED', 'CHECKING', 'AWAITING_UPDATE_FROM_USER', 'AWAITING_UPDATE_FROM_VENDOR', 'FOLLOW_UP' ]
-  field :closing_status, type: Array # %w(BILLING_RATE_NOT_NEGOTIATED UNDER_QUALIFIED INTERVIEW_FAILED APPROVED)
+  field :status, type: Array, default: [] # [ 'APPLIED', 'CHECKING', 'AWAITING_UPDATE_FROM_USER', 'AWAITING_UPDATE_FROM_VENDOR', 'FOLLOW_UP' ]
+  field :closing_status, type: Array, default: [] # %w(BILLING_RATE_NOT_NEGOTIATED UNDER_QUALIFIED INTERVIEW_FAILED APPROVED)
   field :hide, type: Boolean, default: false
 
   belongs_to :consultant, class_name: 'Consultant'
