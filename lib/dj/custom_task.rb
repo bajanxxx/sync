34a5fb554c790155a772bff191564bbcc8798b6a
ashrithr <ashrith@me.com>
2014-06-22 23:32:45 -0700

    Add delayed_job tasks

---
+++ lib/dj/custom_task.rb
@@ -0,0 +1,23 @@
+# Usage: Delayed::Job.enqueue CustomTask.new('lorem ipsum...')
+class CustomTask < Struct.new(:text)
+  def perform
+    say "Performing simple task of printing out #{text}"
+  end
+
+  def success
+    say "sucessfully completed task"
+  end
+
+  def failure
+    say "something went wrong notify someone"
+  end
+
+  # overrides the Delayed::Worker.max_attempts only for this job
+  def max_attempts
+    3
+  end
+
+  def say(text)
+    Delayed::Worker.logger.info("#{Time.now.strftime('%FT%T%z')}: [#{self.class.name} (pid: #{Process.pid})] #{text}")
+  end
+end
