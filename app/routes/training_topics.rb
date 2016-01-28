694a6fd928f1efa10a4043dc3e1098acf0717a6e
Ashrith Mekala <ashrith@me.com>
2016-01-28 15:53:50 -0800

    Training Portal: Fixes Trainer views

--- app/routes/training_topics.rb
+++ app/routes/training_topics.rb
@@ -7,7 +7,7 @@ module Sync
         topic = TrainingTopic.find(topicid)
         consultant = Consultant.find(@session_username)
 
-        if @user.owner? || @user.administrator?
+        if @user.owner? || @user.administrator? || @user.trainer?
           order = if topic.training_sub_topics.count == 0
                     1
                   else
